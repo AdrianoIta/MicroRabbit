813f54904046943dd082b6acc847991768cff17b
Adriano Santos <Adriano.Santos@csgi.com>
2020-05-08 15:47:15 -0300

    Initial commit of infrastructure dependency project.

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Bus.Interfaces;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+
+    }
+}
