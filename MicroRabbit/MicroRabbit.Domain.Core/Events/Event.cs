d59a82c7e0d955690c855a96b9d790ecfc8946a1
Adriano Santos <Adriano.Santos@csgi.com>
2020-05-07 22:39:53 -0300

    Implemented interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        public Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
