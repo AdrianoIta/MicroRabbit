d2fb2d00c14f5bc04bc850941b926bb2fcdcf868
Adriano Santos <Adriano.Santos@csgi.com>
2020-05-12 00:03:17 -0300

    Implemented publish and sendCommand for banking microservice.

--- MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MicroRabbit.Banking.Application.Interfaces;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
 using MicroRabbit.Banking.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace MicroRabbit.Banking.Api.Controllers
 {
@@ -19,11 +17,18 @@ namespace MicroRabbit.Banking.Api.Controllers
             _accountService = accountService;
         }
 
-        // GET api/banking
         [HttpGet]
         public ActionResult<IEnumerable<Account>> Get()
         {
             return Ok(_accountService.GetAccounts());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        {
+            _accountService.Transfer(accountTransfer);
+
+            return Ok(accountTransfer);
         }
     }
 }
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbit.Domain.Core.Bus.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+
+        public TransferCommandHandler(IEventBus bus)
+        {
+            _bus = bus;
+        }
+
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            var transfer = new TransferCreatedEvent(request.From, request.To, request.Amount);
+
+            _bus.Publish(transfer);
+
+            return Task.FromResult(true);
+        }
+    }
+}
